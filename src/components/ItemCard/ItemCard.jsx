7bdef7a16d272b38f72c64d79fbacf2b62059fb8
Victor Kindrat <kindratvictor07@gmail.com>
2023-02-01 19:02:23 +0200

    cards update

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,5 +1,9 @@
 import "./ItemCard.css"
 
+import arrows from "./images/circleArrow.svg"
+import heart from "./images/heart.svg"
+import cart from "./images/cart.svg"
+
 function ItemCard(props) {
     return (
         <div className="ItemCard" data-hot={props.hot} data-discount={props.discount}>
@@ -7,6 +11,17 @@ function ItemCard(props) {
             <div className="ItemCard-description">
                 <div className="ItemCard-name">{props.name}</div>
                 <div className="ItemCard-price">{props.price}</div>
+            </div>
+            <div className="ItemCard-btn-menu">
+                <button className="ItemCard-btn">
+                    <img src={arrows} alt="swipe" className="ItemCart-img" />
+                </button>
+                <button className="ItemCard-btn">
+                    <img src={heart} alt="swipe" className="ItemCart-img" />
+                </button>
+                <button className="ItemCard-btn">
+                    <img src={cart} alt="swipe" className="ItemCart-img" />
+                </button>
             </div>
         </div>
     )
